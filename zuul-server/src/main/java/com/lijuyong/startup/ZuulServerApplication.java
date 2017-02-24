6b4ced9b6982773c2129ceeaa2a09ced99fb7b55
lijuyong <lijuyong@youbangsoft.com>
2017-02-24 16:18:47 +0800

    增加一个api网管，暂时没有使用

---
+++ zuul-server/src/main/java/com/lijuyong/startup/ZuulServerApplication.java
@@ -0,0 +1,15 @@
+package com.lijuyong.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
